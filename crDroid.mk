f52bcf246da72a63d13615d2eb64e56669d5b3e8
naz664 <nazimnavas145@gmail.com>
2023-12-13 16:02:41 +0000

    crDroid-A14: Initial rebase
    Signed-off-by: naz664 <nazimnavas145@gmail.com>

--- crDroid.mk
+++ crDroid.mk
@@ -9,8 +9,8 @@ TARGET_NO_KERNEL_IMAGE := true
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # OTA
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.system.ota.json_url=https://raw.githubusercontent.com/naz664/crDroid_gsi/13/ota.json
+# PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+#    ro.system.ota.json_url=https://raw.githubusercontent.com/naz664/crDroid_gsi/14/ota.json
 
 # Set Bootanimation at 720P
 TARGET_BOOT_ANIMATION_RES := 720
